bd5709405e1bf338584341c620ae1d2295d9e1ab
emergingcoders9 <emergingcoders9@gmail.com>
2023-03-20 17:20:56 +0530

    third

--- src/App.js
+++ src/App.js
@@ -1,33 +1,61 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import { io } from "socket.io-client";
+import { Button, Container, Form } from 'react-bootstrap'
+
 
 function App() {
 
- const socket = io.connect('http://localhost:5000')
+ const socket = io.connect('http://192.168.1.208:5000')
+ const [message, setMessage] = useState("")
+ const  [chat, setChat] = useState([])
 
- const onClick = () =>{
- let data = "hello socket"
+//  const onClick = () =>{
+//  let data = "hello socket"
 
-  socket.emit("message",data)
- }
+//   socket.emit("message",data)
+//  }
 
- useEffect(() => {
+//  useEffect(() => {
 
-  socket.on("reply",(reply)=>{
-    console.log(reply)
-
+//   socket.on("reply",(reply)=>{
+//     console.log(reply)
     
-  })
+//   })
    
- }, [socket])
+//  }, [socket])
+
+const sendMessage = (e) =>{
+  e.preventDefault()
+  socket.emit("message",message)
+  setMessage("")
+}
  
+  socket.on("newMessage",(newMessage)=>{
+    setChat(prev => [...prev,newMessage])
+  })
+
 
 
   return (
-    <div className="App">
-    <button onClick={onClick} >click</button>
+    <div className="App d-flex align-items-center justify-content-center">
+      <Container className='d-flex align-items-center justify-content-center flex-column'>
+        <div className='chatbox w-50 h-50 shadow border-4 rounded-4 p-4'>
+          {chat.map((msg,index)=> {
+            return <div key = {index}>{msg}</div>
+          } )}
+        </div>
+        <Form className='d-flex'>
+          <Form.Group className='mt-3 w-100 flex-grow-1'>
+            <Form.Control type = "text" value={message} onChange = {(e)=> setMessage(e.target.value)}  />
+          </Form.Group>
+          <Button type='submit' onClick={sendMessage} className='mt-3 ms-3'>send</Button>
+        </Form>
+
+
+      </Container>
+    
     </div>
   );
 }
