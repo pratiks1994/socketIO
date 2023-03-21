922685222dddfabd2c8b23a37f94dddf24911fff
emergingcoders9 <emergingcoders9@gmail.com>
2023-03-21 10:40:45 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { useEffect,useState } from 'react';
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 import { Button, Container, Form } from 'react-bootstrap'
 
 
@@ -28,18 +28,36 @@ function App() {
 
 const sendMessage = (e) =>{
   e.preventDefault()
-
   let id = "john"
-
   let newMessage = {id,message}
   socket.emit("message",newMessage)
+  console.log("msg sent")
   setMessage("")
+
 }
  
 
-  socket.on("newMessage",(newMessage)=>{
-    setChat(prev => [...prev,newMessage])
+useEffect(() => {
+
+  socket.on("newMessage", (data)=>{
+    console.log("ran")
+     setChat((prev) => [...prev,data])
+    console.log(chat)
   })
+
+  return () => {
+    socket.off("newMessage")
+  }
+
+  
+}, [chat])
+
+
+  // socket.on("newMessage",(newMessage)=>{
+  //   console.log("ran")
+  //   setChat(prev => [...prev,newMessage])
+  //   // console.log(chat)
+  // })
 
 
 
@@ -47,8 +65,8 @@ const sendMessage = (e) =>{
     <div className="App d-flex align-items-center justify-content-center">
       <Container className='d-flex flex-column justify-content-center align-items-lg-center bg'>
         <div className='chatbox w-50 shadow border-4 rounded-4 p-4'>
-          {chat.map((msg,index)=> {
-            return <div className ={msg.id==="john" ? "text-end m-2 bg-primary rounded p-2" : "text-start m-2 bg-inforounded p-2"}key = {index}>{msg.message}</div>
+          {chat.map((msg)=> {
+            return <div className ={msg.id==="john" ? "text-end m-2 bg-primary rounded p-2" : "text-start m-2 bg-info rounded p-2"}>{msg.message}</div>
           } )}
         </div>
         <Form className='d-flex w-100 justify-content-center'>
